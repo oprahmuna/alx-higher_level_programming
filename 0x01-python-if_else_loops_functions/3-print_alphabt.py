0a8cd3654528d6356eaa1a084d412a970bf4d076
opara samuel <oparachima45@gmail.com>
2023-12-01 21:41:17 -0800

    3-print_alphabt.py

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 for i in range(97, 123):
-    if i == e and i == q
-    continue
-    print("{}".format(chr(i)), end='')
+      if i != 101 and i != 113:
+                  print("{}".format(chr(i)), end='')
